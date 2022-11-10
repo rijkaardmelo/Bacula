fa8bf000fb867a6db9a3ef4bc79403298a226d90
Rijkaard Melo <rijkaardmelo@gmail.com>
2022-11-10 09:56:39 -0300

    main

--- Install.sh
+++ Install.sh
@@ -14,19 +14,17 @@ rm Bacula-4096-Distribution-Verification-key.asc
 
 cd /
 
-echo "deb [arch=amd64] https://bacula.org/packages/5f1e8eefd1016/bacula/debs/13.0.1 bullseye main" > /etc/apt/sources.list.d/Bacula-Community.list
+echo "deb [arch=amd64] https://bacula.org/packages/5f1e8eefd1016/bacula/debs/13.0.1 jammy main" > /etc/apt/sources.list.d/Bacula-Community.list
 
-apt update && apt -y install bacula-postgresql php7.4-pgsql 
+apt update && apt -y install bacula-postgresql php-pgsql 
 
-# Install Baculum in Debian 11
+# Install Baculum
 wget -qO - http://www.bacula.org/downloads/baculum/baculum.pub | apt-key add -
 
-echo "deb http://www.bacula.org/downloads/baculum/stable-11/debian bullseye main
-deb-src http://www.bacula.org/downloads/baculum/stable-11/debian bullseye main" > /etc/apt/sources.list.d/baculum.list
+echo "deb [ arch=amd64 ] http://www.bacula.org/downloads/baculum/stable-11/ubuntu focal main
+deb-src http://www.bacula.org/downloads/baculum/stable-11/ubuntu focal main" > /etc/apt/sources.list.d/baculum.list
 
-apt update && apt -y install php7.4-bcmath php7.4-mbstring php7.4-dom php7.4-curl php7.4-ldap baculum-common baculum-api baculum-api-apache2
-
-apt update && apt -y install baculum-common baculum-web baculum-web-apache2
+apt update && apt -y install php-bcmath php-mbstring php-dom php-curl php-ldap baculum-common baculum-api baculum-api-apache2 baculum-web baculum-web-apache2
 
 a2enmod rewrite
 
@@ -41,6 +39,6 @@ www-data ALL = (root) NOPASSWD: /opt/bacula/bin/bsdjson
 www-data ALL = (root) NOPASSWD: /opt/bacula/bin/bfdjson
 www-data ALL = (root) NOPASSWD: /opt/bacula/bin/bbconsjson" > /etc/sudoers.d/baculum-api
 
-chown www-data -R /opt/bacula/working
+# chown www-data -R /opt/bacula/working
 
 systemctl restart apache2
